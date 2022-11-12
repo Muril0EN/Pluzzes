a913a9d695a1b2cfc06264699a6c74d7afb122e5
Murilo <muril0.esteves@protonmail.com>
2022-11-11 20:15:43 -0400

    adicionando pasta de jogos em C

---
+++ adivinhacao.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main (){
+	printf("************************************\n");
+	printf("* Bem vindo ao jogo de adivinhação *\n");
+	printf("************************************\n");
+//comentário de linha 	
+	
+	int numeroSecreto = 3;
+	
+	int chute;
+	
+	printf("Digite seu chute:");
+	scanf("%d", &chute);
+
+	int acertou = (chute == numeroSecreto);
+
+	if(acertou){
+		printf("Você acertou, parabéns!\n\n");
+	} else {
+		int maior = (numeroSecreto < chute);
+		if (maior){
+			printf("Você errou, seu chute foi maior que o número secreto.\n\n");
+		} else {
+			printf("Você errou, seu chute foi menor que o número secreto.\n\n");
+		}
+	}
+}
---
+++ letsMakeDeal.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    printf("*********************\n");
+    printf("* Let's make a deal  \n*");
+    printf("*********************\n\n");
+
+    int chute;
+    int portaPremiada;
+    int portaErrada;
+
+    printf("Atrás de uma dessas portas há uma barra de ouro, \n atrás das outras duas, duas lixeiras\n\n");
+
+    srand(time(0));
+    portaPremiada = (rand()%3) + 1;
+    printf("Porta premiada: %d\n\n", portaPremiada); //teste
+
+    printf("Escolha a porta (1, 2 ou 3):");
+    scanf("%d", &chute);
+
+    if (portaPremiada == chute){
+        printf("Parabéns, você acertou!\n");
+    } 
+    else {
+        printf("Ops, você errou, tente novamente.\n");
+    }
+}
