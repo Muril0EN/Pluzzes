0844ff157ccaf59eec56c7fe1a1f6a9d4e0f1a6a
Murilo <muril0.esteves@protonmail.com>
2022-11-21 09:02:47 -0400

    implementando o loop de verificação e usando a função strlen (tamanho do array)

--- forca.c
+++ forca.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main () {
 //não existem strings na linguagem C! 
@@ -8,7 +9,23 @@ int main () {
 // sintaxe para atribuir-> nome [posição] = 10; (semelhante para imprimir) 
     char palavraSecreta [20];
 //imprimir array
-    sprintf(palavraSecreta, "melancia"); //função para simular string
-    printf("%s\n", palavraSecreta); //%s usado para imprimir string de char 
+    sprintf(palavraSecreta, "melancia"); //função que ajuda a escrever palavras dentro de arrays de chars
+    //printf("%s\n", palavraSecreta); //%s usado para imprimir array de char 
+
+    int acertou = 0;
+    int enforcou = 0;
+
+    do {
+        char chute;/* code */
+        scanf("%c", &chute);
+
+        for(int i = 0; i < strlen(palavraSecreta); i++) {
+            if(palavraSecreta[i] == chute){
+                printf("A posição %d tem essa letra!\n", i);
+            }
+        }
+    } while (!acertou && !enforcou); // = (acertou == 0 && enforcou == 0), isso pq '!' é operador de negação.
     
+    
+
 }
