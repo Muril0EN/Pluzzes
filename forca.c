813b303473a36252d3a96a448fc426c36abf37ef
Murilo <muril0.esteves@protonmail.com>
2022-11-27 10:00:49 -0400

    excluindo comentários redundantes

--- forca.c
+++ forca.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 #include <string.h>
-
-//declaração da função -> funções facilitam a leitura manutenção, reutilização de códigos 
+ 
 void abertura () {
     printf("********************************\n");
     printf("*         Jogo de forca        *\n");
     printf("********************************\n\n");
 }
-//declaração da função fora da função principal
-void chuta (char chutes[26], int* tentativas) { //sintaxe para declaração de parametros -> tipo nome
+
+void chuta (char chutes[26], int* tentativas) { 
     char chute;
-    scanf(" %c", &chute); //comando para ignorar o 'enter' pq fica o 'enter' fica no buffer (só necessário em algoritmos com char)
+    scanf(" %c", &chute); 
 
     chutes[*tentativas] = chute;
-    (*tentativas)++; //incremento no conteúdo ponteiro
-} //**** a razão para a variável chutes não ter sido reescrita é pq um array já é um ponteiro.
-//funcções 'void' não devolve nada. Aqui precisamos devolver um inteiro
+    (*tentativas)++; 
+} 
 int jaChutou (char letra, char chutes [26], int tentativas){
     int achou = 0;
     
@@ -31,9 +29,8 @@ int jaChutou (char letra, char chutes [26], int tentativas){
 
 void desenhaForca(char palavraSecreta[20], char chutes [26], int tentativas) {
 
-    for (int i = 0; i < strlen(palavraSecreta); i++) {//função strlen devolve o tamanho do array
+    for (int i = 0; i < strlen(palavraSecreta); i++) {
             
-            //retorno da função jaChutou atribuído à variável achou;
             int achou = jaChutou(palavraSecreta[i], chutes, tentativas);
 
             if(achou){
@@ -53,24 +50,20 @@ int main () {
  
     char palavraSecreta [20];
     
-     //função que ajuda a escrever palavras dentro de arrays de chars
-    //printf("%s\n", palavraSecreta); //%s usado para imprimir array de char 
-
     int acertou = 0;
     int enforcou = 0;
 
-    char chutes[26]; //tamanho do alfabeto estendido
+    char chutes[26]; 
     int tentativas = 0; 
 
     escolhePalavra (palavraSecreta);
-    abertura(); //chamada de função de clarada na linha 4;
+    abertura(); 
 
     do {
 
         desenhaForca(palavraSecreta, chutes, tentativas);
-        chuta(chutes, &tentativas); //para usar o endereço de memória da variável tentativas, para entregar o valor desse endereço para a função
-        //tentativas++;//esse passo deve estar dentro da função (boa prática)
+        chuta(chutes, &tentativas); 
 
-    } while (!acertou && !enforcou); // = (acertou == 0 && enforcou == 0), isso pq '!' é operador de negação.
+    } while (!acertou && !enforcou);
     
 }
