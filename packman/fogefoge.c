a424d4b47aaf76037be448ea89937f263b804bb2
Murilo <muril0.esteves@protonmail.com>
2022-12-07 23:55:17 -0400

    implementando função que lê e função que imprime o mapa

--- packman/fogefoge.c
+++ packman/fogefoge.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
     
-//como não declarar a matriz    
-    /*char linha1[10];
-    char linha2[10];
-    char linha3[10];
-    char linha4[10];
-    char linha5[10];*/
-//maneira correta de declarar a matriz ->  tipo nome [linhas][colunas];
-    char mapa[5][10];
-//manipular -> nome [indice] [indice]
-    mapa[0][9] = '|';
-    mapa[4][9] = '@';
-//imprimir elemento da matriz -> printf(%tipo, nome[indice][indice])
-    printf("%c %c\n", mapa[0][9], mapa[4][9]);
+    char mapa[5][10+1];
+
+    FILE* f;
+    f = fopen("mapa.txt", "r");
+    if(f == 0) {
+        printf("Erro na leitura do mapa!\n");
+        exit(1);
+    }
+
+    for(int i = 0; i < 5; i++){
+        fscanf(f, "%s", mapa[i]);
+    }
+
+    for (int i = 0; i < 5; i++){
+        printf("%s\n", mapa[i]);
+    }
+
+    fclose(f);
+    
 }
