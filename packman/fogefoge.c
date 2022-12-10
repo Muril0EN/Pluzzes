f689e648ad06a74addd07fe3e658be4edddc6761
Murilo <muril0.esteves@protonmail.com>
2022-12-10 08:30:44 -0400

    organização da pasta de jogos em C

--- packman/fogefoge.c
+++ packman/fogefoge.c
@@ -3,23 +3,23 @@
 
 int main(){
     
-    char mapa[5][10+1];
+    char mapa[5][10+1];// a declaração de 10+1 pq a string termina com \0; 
 
-    FILE* f;
-    f = fopen("mapa.txt", "r");
-    if(f == 0) {
+    FILE* arquivo; //nome do arquivo
+    arquivo = fopen("mapa.txt", "r");
+    if(arquivo == 0) {
         printf("Erro na leitura do mapa!\n");
         exit(1);
     }
 
     for(int i = 0; i < 5; i++){
-        fscanf(f, "%s", mapa[i]);
+        fscanf(arquivo, "%s", mapa[i]);//lê a linha inteira e passa para o mapa a cada loop
     }
 
     for (int i = 0; i < 5; i++){
-        printf("%s\n", mapa[i]);
+        printf("%s\n", mapa[i]);//imprime mapa
     }
 
-    fclose(f);
+    fclose(arquivo);
     
 }
