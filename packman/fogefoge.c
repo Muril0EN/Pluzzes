4da0b43e471eb2c78fc6d28c556bf327d82c2b4f
Murilo <muril0.esteves@protonmail.com>
2022-12-11 14:20:30 -0400

    refatorando e criando funções

--- packman/fogefoge.c
+++ packman/fogefoge.c
@@ -5,8 +5,21 @@ char** mapa; //declaração de ponteiros de ponteiros (necessário para matriz)
 int linhas;
 int colunas; 
 
-int main(){
-    
+void liberaMapa (){
+    for(int i = 0; i < linhas; i++){//
+        free(mapa[i]);//liberar memória de cada uma das linhas (ponteiros)
+    }
+    free(mapa);
+}
+
+void alocaMapa(){
+    mapa = malloc(sizeof(char*) * linhas);//malloc -> alocação dinâmica de mem. / sizeoff -> informa qtd de bytes cada tipo de var precisa / * -> multiplica quantidade  que vai 'entrar'
+    for(int i = 0; i < linhas; i++){// para cada linha
+        mapa[i] = malloc(sizeof(char) * (colunas + 1)); //alocar 'vezes' quantidade de colunas 
+    }
+}
+
+void leMapa (){
     FILE* arquivo; //nome do arquivo
     arquivo = fopen("mapa.txt", "r"); //lê arquivo
     if(arquivo == 0) { //trata erro e fecha o programa em caso de problemas com o arquivo
@@ -15,37 +28,22 @@ int main(){
     }
 
     fscanf(arquivo, "%d %d", &linhas, &colunas); //
-    printf("linhas %d colunas %d\n", linhas, colunas);//
 
-    mapa = malloc(sizeof(char*) * linhas);//malloc -> alocação dinâmica de mem. / sizeoff -> informa qtd de bytes cada tipo de var precisa / * -> multiplica quantidade  que vai 'entrar'
-    for(int i = 0; i < linhas; i++){// para cada linha
-        mapa[i] = malloc(sizeof(char) * (colunas + 1)); //alocar 'vezes' quantidade de colunas 
-    }
+    alocaMapa();
 
-    for(int i = 0; i < 5; i++) {// ler o arquivo
+    for (int i = 0; i < 5; i++){
         fscanf(arquivo, "%s", mapa[i]);
     }
+    fclose(arquivo);//fecha o arquivo
+}
 
+int main(){
+
+    leMapa();
+    
     for (int i = 0; i < 5; i++){//imprimir arquivo
         printf("%s\n", mapa[i]);//
     }
     
-    fclose(arquivo);//fecha o arquivo
-
-    for(int i = 0; i < linhas; i++){//
-        free(mapa[i]);//liberar memória de cada uma das linhas (ponteiros)
-    }
-
-    free(mapa);//liberar memória do ponteiro de ponteiro
-
-    for(int i = 0; i < 5; i++){
-        fscanf(arquivo, "%s", mapa[i]);//lê a linha inteira e passa para o mapa a cada loop
-    }
-
-    for (int i = 0; i < 5; i++){
-        printf("%s\n", mapa[i]);//imprime mapa
-    }
-
-    fclose(arquivo);
-    
+    liberaMapa();
 }
